709496432ae2c60ac3f9727c544023335396c24a
Dmitry Moskowski <me@corpix.ru>
2016-06-04 19:15:48 +0000

    Rename other pieces of boilerplate while init

--- init.sh
+++ init.sh
@@ -3,13 +3,25 @@
 set -e
 set -x
 
-package="github.com/corpix/go-boilerplate"
 cwd="$(pwd)"
 
-git clone "https://${package}" .
+boiler_package="github.com/corpix/go-boilerplate"
+boiler_package_base="$(dirname "$boiler_package")"
+boiler_package_name="$(basename "$boiler_package")"
+
+new_package="${cwd/$GOPATH\/src\//}"
+package_base="$(dirname "$new_package")"
+package_name="$(basename "$new_package")"
+
+git clone "https://${boiler_package}" .
 rm -rf init.sh
 
 find . -type f | \
-    xargs perl -p -i -e "s|${package}|${cwd/$GOPATH\/src\//}|g"
+    xargs perl -p -i -e "s|${boiler_package}|${new_package}|g"
+
+perl -p -i -e "s|${boiler_package_base}|${package_base}|g" GNUmakefile
+perl -p -i -e "s|${boiler_package_name}|${package_name}|g" GNUmakefile
+
+mv "${boiler_package_name}" "${package_name}"
 
 echo Done.
